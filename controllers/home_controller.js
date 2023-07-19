19733bb2581530258d3e62b49cd02c4e01d441f4
Vinayak Sannaik <112576218+Vinayak-Sannaik@users.noreply.github.com>
2023-07-19 12:47:29 +0530

    Update home_controller.js

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,7 @@
 const Project = require('../models/project');
 module.exports.home = async function (req, res) {
   try {
-    let projects = await Project.find({}).sort('-createdAt');
+    const projects = await Project.find({}).sort('-createdAt');
     return res.render('home', {
       title: 'Issue Tracker | Home',
       projects,
