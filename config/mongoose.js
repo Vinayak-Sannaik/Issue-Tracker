4218bf78eb88194c81c076d6658d371630fd8b12
Vinayak Sannaik <112576218+Vinayak-Sannaik@users.noreply.github.com>
2023-07-19 14:21:00 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://0.0.0.0:27017/db';
+// const url = 'mongodb://0.0.0.0:27017/db';
+const url = 'mongodb+srv://root:C581Vxk7HrfsIKgQ@cluster0.ahwe9q0.mongodb.net/';
 // Set the 'strictQuery' option to 'false' to suppress the warning
 mongoose.set('strictQuery', false);
 const db = async () => {
