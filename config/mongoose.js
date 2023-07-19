bc453c9d5edf68485e8004ade2a049dcd15c6f64
Vinayak Sannaik <112576218+Vinayak-Sannaik@users.noreply.github.com>
2023-07-19 13:02:02 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,16 +1,14 @@
 const mongoose = require('mongoose');
 const url = 'mongodb://0.0.0.0:27017/db';
-
 // Set the 'strictQuery' option to 'false' to suppress the warning
 mongoose.set('strictQuery', false);
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
-db.once('open', () => {
-  console.log("Connected to DB!");
-});
-
-module.exports = db;
+const db = async () => {
+  try {
+    await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('Connected to DB!');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
+db();
+module.exports = mongoose.connection;
