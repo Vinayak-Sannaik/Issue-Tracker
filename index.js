a2a3bb9d2b71d2f35f2ef527e82d649e13822508
Vinayak Sannaik <112576218+Vinayak-Sannaik@users.noreply.github.com>
2023-07-19 12:49:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,9 +21,6 @@ app.set('views', './views');
 
 // use express router
 app.use('/', require('./routes'));
-app.listen(port, function (err) {
-  if (err) {
-    console.log(`Error in running the server: ${err}`);
-  }
-  console.log(`Server is running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
